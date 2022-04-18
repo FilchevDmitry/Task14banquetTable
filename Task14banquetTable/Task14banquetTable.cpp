07ec7d3ae0c67d9b2051b0f280c2954b22f6ac25
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-18 16:22:39 +0300

    'ok'

--- Task14banquetTable/Task14banquetTable.cpp
+++ Task14banquetTable/Task14banquetTable.cpp
@@ -1,7 +1,68 @@
 ﻿#include <iostream>
-
+#include<vector>
 int main()
 {
-    std::cout << "Hello World!\n";
+	int cutlery[2][6] = {{4,3,3,3,3,3},
+						 {4,3,3,3,3,3}};
+	int plates[2][6] = {{3,2,2,2,2,2},
+						{3,2,2,2,2,2}};
+	int chair[2][6] = {{1,1,1,1,1,1},
+					   {1,1,1,1,1,1}};
+	for (int i=0; i<2;i++ )
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << cutlery[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << plates[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << chair[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	chair[0][4] = 2;
+	plates[1][0] = 2;
+	cutlery[1][0] = 3;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << cutlery[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << plates[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << chair[i][j];
+		}
+		std::cout << std::endl;
+	}
 }
 
